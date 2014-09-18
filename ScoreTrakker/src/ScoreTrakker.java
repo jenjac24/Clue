5f9f5a5e2344c011ed019da868d43a7c8a5a4755
Jennifer Jacobs <jejacobs@mymail.mines.edu>
2014-09-18 16:01:43 -0600

    followed rubric this time, updated exception handling

--- ScoreTrakker/src/ScoreTrakker.java
+++ ScoreTrakker/src/ScoreTrakker.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 
 public class ScoreTrakker {
 	private ArrayList<Student> students;
+	private String[] files = {"scores.txt", "badscore.txt", "nofile.txt" };
 
 	public ScoreTrakker() {
 		// TODO Auto-generated constructor stub
@@ -15,42 +16,46 @@ public class ScoreTrakker {
 	public void loadDataFromFile(String fileName) throws FileNotFoundException{
 		students = new ArrayList<Student>();
 
-			FileReader read = null;
-			Scanner scan = null;
-			Scanner scanInt = null;
-			String strNum = "", name = "";
-			int score = 0;
+		FileReader read = null;
+		Scanner scan = null;
+		Scanner scanInt = null;
+		String strNum = "", name = "";
+		int score = 0;
+		while(scan.hasNextLine()){
 			try {
 				read = new FileReader(fileName);
 				scan = new Scanner(read);
-				while(scan.hasNextLine()){
-					name = scan.nextLine();
-					strNum = scan.nextLine();
-					score = Integer.parseInt(strNum);
-					students.add(new Student(name, score));
-				}
+
+				name = scan.nextLine();
+				strNum = scan.nextLine();
+				score = Integer.parseInt(strNum);
+				students.add(new Student(name, score));
 			}
+
 			catch(NumberFormatException n){
 				System.out.println("Incorrect format for " + name + " not a valid score: " + n.getMessage());
 			}
+		}
 	}
 
 	public void printInOrder(){
 		Collections.sort(students);
 		for(Student s: students)
 			System.out.println(s);
-		
+
 	}
-	
+
 	public void processFiles(){
-		try{
-			loadDataFromFile("scores.txt");
-		}catch(FileNotFoundException e){
-			System.out.println("Can't open file: " + e.getMessage());
+		for(String file: files){
+			try{
+				loadDataFromFile(file);
+				printInOrder();
+			}catch(FileNotFoundException e){
+				System.out.println("Can't open file: " + e.getMessage());
+			}
 		}
-		printInOrder();
 	}
-	
+
 	public static void main(String[] args) {
 		ScoreTrakker score = new ScoreTrakker();
 		score.processFiles();
