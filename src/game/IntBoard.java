887f725bd4b29fe7e87db55b5bc2e8e6b0f26b14
QuentCorich <qcorich@mymail.mines.edu>
2014-09-25 12:08:50 -0600

    Worked on calcTargets

--- src/game/IntBoard.java
+++ src/game/IntBoard.java
@@ -1,18 +1,26 @@
 package game;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Set;
 
 public class IntBoard {
 	
-	
+	private Set<BoardCell> visited;
+	private Set<BoardCell> targets;
 	
 	public IntBoard(){
 		Set targets = null;
 	}
 	
-	public Set<BoardCell> calcTargets(BoardCell cell, int numberOfMoves){
-		
+	public Set<BoardCell> calcTargets(BoardCell thisCell, int numberOfMoves){ //finds possible moves
+		visited.add(thisCell);
+		LinkedList<BoardCell> adjCell = null; 
+		adjCell = getAdjList(thisCell); //may need a try catch?
+		for(BoardCell c : adjCell){
+			if(numberOfMoves == 1) targets.add(c); //if last move used, saves spot as possible location
+			else calcTargets(c, numberOfMoves --); //spends one move to an adjacent cell
+		}
 		return null;
 	}
 	
