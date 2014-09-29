96b0e1db1828452acac4774775f02fd69a6d5af7
Alex Bieman <abieman@mines.edu>
2014-09-29 15:50:28 -0600

    changed layout to be a BoardCell array, room legend map still not passing test

--- src/game/Board.java
+++ src/game/Board.java
@@ -1,18 +1,16 @@
 package game;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Board {
-	private String[][] layout; //size of layout will be determined upon reading file
+	private BoardCell[][] layout; //size of layout will be determined upon reading file
 	private Map<Character,String> rooms;
 	private int numRows, numColumns;
 	//Added code that reads in the legend and correctly creates the map.
-	public static void loadBoardConfig(String layout, String legend) throws Exception{
+	public void loadBoardConfig(String layout, String legend) throws Exception{
 		Map<Character,String> rooms = new HashMap<Character,String>();
 		FileReader reader = new FileReader(legend);
 		Scanner in = new Scanner(reader);
@@ -29,7 +27,6 @@ public class Board {
 	public int getNumColumns() {
 		return numColumns;
 	}
-	// getRooms is generating a null pointer exception in testRooms
 	public Map<Character,String> getRooms() {
 		return rooms;
 	}
@@ -39,7 +36,7 @@ public class Board {
 	public String getRoom(Character key) {
 		return rooms.get(key);
 	}
-	public String[][] getLayout() {
+	public BoardCell[][] getLayout() {
 		return layout;
 	}
 }
--- src/game/ClueGame.java
+++ src/game/ClueGame.java
@@ -6,17 +6,19 @@ public class ClueGame {
 	private Map<Character,String> rooms;
 	private String layout;
 	private String legend;
+	private Board board;
 	
 	public ClueGame(String a, String b){
 		layout = a;
 		legend = b;
+		board = new Board();
 	}
 	
 	public void loadConfigFiles() throws Exception{
-		Board.loadBoardConfig(layout,legend);
+		board.loadBoardConfig(layout,legend);
 	}
 
 	public Board getBoard() {
-		return null;
+		return board;
 	}
 }
